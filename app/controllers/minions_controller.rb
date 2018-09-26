31af9c851f5c0bb02b78a893a9af4d28bc2b5dad
Jose Manuel Rodriguez <jmrv002@gmail.com>
2018-09-26 08:13:33 -0400

    add backend to get the result

--- app/controllers/minions_controller.rb
+++ app/controllers/minions_controller.rb
@@ -1,5 +1,34 @@
 class MinionsController < ApplicationController
   def index
-  	name = 'joel'
+    name = 'joel'
+  end
+
+  def get_result
+    userOption = params[:userOption]
+    minionsOption = getMinionsOption()
+
+    result = user_win?(userOption, minionsOption)
+
+    render json: {
+      :user => userOption, 
+      :minions => minionsOption, 
+      :result => result
+    }
+  end
+
+  def getMinionsOption
+    return ['rock', 'paper', 'scissor'].sample
+  end
+
+  def user_win?(userOption, minionsOption)
+    return case
+      when userOption == 'rock' && minionsOption == 'scissor' then true
+      when userOption == 'rock' && minionsOption == 'paper' then false
+      when userOption == 'paper' && minionsOption == 'rock' then true
+      when userOption == 'paper' && minionsOption == 'scissor' then false
+      when userOption == 'scissor' && minionsOption == 'paper' then true
+      when userOption == 'scissor' && minionsOption == 'rock' then false
+      when userOption == minionsOption then nil
+      end 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	root 'minions#index'
   get 'minions/index'
+	get 'minions/result', to: 'minions#get_result'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
