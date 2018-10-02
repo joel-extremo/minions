83ff04048cc8150eb273b8ca83ec5b580fcaf279
Jose Manuel Rodriguez <jmrv002@gmail.com>
2018-10-01 22:17:46 -0400

    independent files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,6 +13,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
+Rails.application.config.assets.precompile += %w( game.css )
+Rails.application.config.assets.precompile += %w( game.js )
+
 # modal
 Rails.application.config.assets.precompile += %w( modal.css )
 Rails.application.config.assets.precompile += %w( modal.js )
